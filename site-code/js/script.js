2eb86d3770f90aeec1a75ff07fcd6488d3292a7c
dgoudie <34222208+dgoudie@users.noreply.github.com>
2018-04-20 09:43:09 -0500

    Added Documentation

--- site-code/js/script.js
+++ site-code/js/script.js
@@ -55,10 +55,7 @@ app.controller('Home_Controller', function ($scope) {
                 }
 
 
-<<<<<<< HEAD
-=======
                 // Not sure what this does, but it fixed an issue I was having
->>>>>>> Daniel
                 $scope.$applyAsync();
 
                 // Fades out the progress ring that appears when loading the page
