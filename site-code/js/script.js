79b884c7de6df2c394e1bfcba901e1d1c4783af4
dgoudie <34222208+dgoudie@users.noreply.github.com>
2018-04-19 22:37:29 -0500

    Added documentation

--- site-code/js/script.js
+++ site-code/js/script.js
@@ -15,13 +15,17 @@ app.config(function ($routeProvider) {
             templateUrl: '../home.html',
             controller: 'Home_Controller'
         })
+        .when('/about', {
+            templateUrl: '../about.html'
+        })
         .otherwise({
             redirectTo: '/'
         })
 });
 
 app.controller('Home_Controller', function ($scope) {
-    $('.top-arrow').hide();
+
+    // For testing
     $(document).ready(setTimeout(function () {
         $.ajax({
             async: true,
@@ -30,12 +34,31 @@ app.controller('Home_Controller', function ($scope) {
             success: callback()
         });
 
+        // this function is called right after the ajax request is complete
         function callback() {
             return function (data, textStatus, jqXHR) {
+                
+                // "data" variable is returned by ajax request
+
+                // use the $scope variable to link variables between here and the HTML
+                // $scope.launches would be accessible inside HTML as the "launches" variable (an array in this example)
                 $scope.launches = data.launches;
+
+                // Get weather data here
+                for (var i = 0; i < $scope.launches.length; i++) {
+                    
+                    // Parse currentLaunch JSON object to find location
+
+                    // Get weather data based on location (maybe based on lat/long)
+
+                    // launches[i].weather = ajaxreturn weather object
+                }
+
+
                 $scope.$applyAsync();
                 $('#loader').fadeOut(300);
             }
         }
     }, 500));
+
 });
