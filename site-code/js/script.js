5ba284e78a1fa9d613c020e87ccd0b7e9a1c58f3
dgoudie <34222208+dgoudie@users.noreply.github.com>
2018-04-19 21:41:13 -0500

    Added small formatting eye candy

--- site-code/js/script.js
+++ site-code/js/script.js
@@ -2,40 +2,10 @@
 // create the module and name it app
 var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngMaterial', 'ngMessages']);
 
-// app.config(function ($mdThemingProvider) {
-
-//     $mdThemingProvider.definePalette('launchHubPalette', {
-//         '50': '547c92',
-//         '100': '4a6e82',
-//         '200': '416071',
-//         '300': '385361',
-//         '400': '2f4551',
-//         '500': '293d48',
-//         '600': '253741',
-//         '700': '1c2931',
-//         '800': '131c20',
-//         '900': '090e10',
-//         'A100': 'ff8566',
-//         'A200': 'ff5c33',
-//         'A400': 'ff3300',
-//         'A700': 'b32400',
-//         'contrastDefaultColor': 'light', // whether, by default, text (contrast)
-//         // on this palette should be dark or light
-
-//         'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-//             '200', '300', '400', 'A100'
-//         ]
-//     });
-
-//     $mdThemingProvider.theme('default')
-//         .primaryPalette('launchHubPalette')
-
-// });
-
 app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('blue-grey')
-        .accentPalette('deep-orange');
+        .accentPalette('orange');
 });
 
 // configure our routes
@@ -51,6 +21,7 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('Home_Controller', function ($scope) {
+    $('.top-arrow').hide();
     $(document).ready(setTimeout(function () {
         $.ajax({
             async: true,
@@ -63,6 +34,8 @@ app.controller('Home_Controller', function ($scope) {
             return function (data, textStatus, jqXHR) {
                 $scope.launches = data.launches;
                 $scope.$applyAsync();
+                $('#homeHeader').text('Launches');
+                $('.top-arrow').fadeIn();
             }
         }
     }, 500));
