3c3b70cbe02d5083e8a097b64640680df6059652
dgoudie <34222208+dgoudie@users.noreply.github.com>
2018-04-19 22:57:16 -0500

    Code Commenting

--- site-code/js/script.js
+++ site-code/js/script.js
@@ -15,13 +15,17 @@ app.config(function ($routeProvider) {
             templateUrl: '../home.html',
             controller: 'Home_Controller'
         })
+        .when('/about', {
+            templateUrl: '../about.html'
+        })
         .otherwise({
             redirectTo: '/'
         })
 });
 
 app.controller('Home_Controller', function ($scope) {
-    $('.top-arrow').hide();
+
+    // For testing
     $(document).ready(setTimeout(function () {
         $.ajax({
             async: true,
@@ -30,12 +34,34 @@ app.controller('Home_Controller', function ($scope) {
             success: callback()
         });
 
+        // this function is called right after the ajax request is complete
         function callback() {
             return function (data, textStatus, jqXHR) {
+                
+                // "data" variable is returned by ajax request
+
+                // use the $scope variable to link variables between here and the HTML
+                // $scope.launches would be accessible inside HTML as the "launches" variable (an array in this example)
                 $scope.launches = data.launches;
+
+                // Get weather data here
+                for (var i = 0; i < $scope.launches.length; i++) {
+                    
+                    // Parse currentLaunch JSON object to find location
+
+                    // Get weather data based on location (maybe based on lat/long)
+
+                    // launches[i].weather = ajaxreturn weather object
+                }
+
+
+                // Not sure what this does, but it fixed an issue I was having
                 $scope.$applyAsync();
+
+                // Fades out the progress ring that appears when loading the page
                 $('#loader').fadeOut(300);
             }
         }
     }, 500));
+
 });
