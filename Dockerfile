76d0cc1d2cc13db6c8a741999e99e84d72e56013
SoarinFerret <thecodye@kanto.cloud>
2018-04-22 00:54:16 -0500

    Remove Windows Line characters due to NPM issues running on both Windows and Unix Dev boxes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:carbon
 
+# Install programs as necessary
+RUN apt-get update && apt-get install dos2unix -y
+
 # Copy app source
 COPY ./site-code /usr/src/app
 
--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@
 # Build the Node JS dependencies
 cd /usr/src/app
 
+# JUST IN CASE, remove Windows Line formats
+find . -type f -print0 | xargs -0 dos2unix
+
 echo "Installing dependencies"
 npm install
 
