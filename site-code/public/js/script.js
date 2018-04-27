0e6a9e1d5b1af2f1514792b29ef5b6b604e34140
Yuqi <yuqikang@unomaha.edu>
2018-04-27 12:24:05 -0500

    add changes

--- site-code/public/js/script.js
+++ site-code/public/js/script.js
@@ -222,7 +222,7 @@ app.controller('Contributors_Controller', function ($scope) {
         description: `Yuqi is a senior at the University of Nebraska at 
 	Omaha studying Management Information Systems and a minor in Computer Science. She will go to 
 	graduate school to study Data Science after and she is passion about 
-	data analysis.`,
+	data analysi.`,
         githubUsername: "yuqikang",
     });
 });
