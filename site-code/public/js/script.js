c4ceacdbd47e4fb5b78bf1bfa523495548a0df3a
Yuqi <yuqikang@unomaha.edu>
2018-04-26 23:46:30 -0500

    add changes

--- site-code/public/js/script.js
+++ site-code/public/js/script.js
@@ -217,7 +217,7 @@ app.controller('Contributors_Controller', function ($scope) {
     }, {
         name: "Yuqi Kang",
         description: `Yuqi is a senior at the University of Nebraska at 
-	Omaha studying Management Information Systems. She will go to 
+	Omaha studying Management Information Systems and a minor in Computer Science. She will go to 
 	graduate school to study Data Science and she is passion about 
 	data analysis.`,
         githubUsername: "yuqikang",
